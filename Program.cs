b059702379c4956514910f65e30eefe4903678a8
HoangCuon910 <161955602+HoangCuon910@users.noreply.github.com>
2025-02-11 22:33:36 +0700

    Doi tien

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MoneyCoverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float rate = 25300f;
+
+            Console.WriteLine("Nhap gia tri USD: ");
+            float usdValue = float.Parse(Console.ReadLine());
+            
+            float vndValue = usdValue * rate;
+
+            Console.WriteLine($"{usdValue} USD = {vndValue} VND.");
+        }
+    }
+}
